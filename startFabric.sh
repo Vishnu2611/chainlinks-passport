acb6a07b5776ee62287b877b9b0b41ee01b080ba
Vishnu Pradeep <vishnu.pradeep@ults.in>
2020-03-07 23:36:37 +0530

    third-commit

--- startFabric.sh
+++ startFabric.sh
@@ -8,3 +8,8 @@
 ./bin/configtxgen -profile Chainlinks_Channel -outputAnchorPeersUpdate ./channel-artifacts/Police_Channelanchors.tx -channelID auctionchannel -asOrg PoliceMSP
 
 ./bin/configtxgen -profile Chainlinks_Channel -outputAnchorPeersUpdate ./channel-artifacts/Airport_Channelanchors.tx -channelID auctionchannel -asOrg AirportMSP
+
+# export CORE_PEER_LOCALMSPID=
+# export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/passportseva.chainlinks.com/peers/peer0.passportseva.chainlinks.com/tls/ca.crt
+# export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/passportseva.chainlinks.com/users/Admin@passportseva.chainlinks.com/msp
+# export CORE_PEER_ADDRESS=peer1.passportseva.chainlinks.com:8051
